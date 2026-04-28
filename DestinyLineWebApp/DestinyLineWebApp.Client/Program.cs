2bf88f09ef6c275b9540b4d3de98a03183ce6d5a
Stephans-1 <kukustephansneriskwei@gmail.com>
2026-04-28 19:44:53 +0000

    404 correction

--- DestinyLineWebApp/DestinyLineWebApp.Client/Program.cs
+++ DestinyLineWebApp/DestinyLineWebApp.Client/Program.cs
@@ -1,7 +1,8 @@
+using DestinyLineWebApp.Client;
 using DestinyLineWebApp.Client.Services;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.Services.AddSingleton<IMemberService, MemberService>();
-
+builder.RootComponents.Add<Routes>("#app");
 await builder.Build().RunAsync();
