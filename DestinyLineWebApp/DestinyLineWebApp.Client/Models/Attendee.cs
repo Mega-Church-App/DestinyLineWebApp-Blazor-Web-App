4636f2a009ac15c7fa2cc0dec0b40895f0153618
Stephans-1 <kukustephansneriskwei@gmail.com>
2026-04-29 01:56:57 +0000

    css and blazor styles

---
+++ DestinyLineWebApp/DestinyLineWebApp.Client/Models/Attendee.cs
@@ -0,0 +1,11 @@
+﻿namespace DestinyLineWebApp.Client.Models
+{
+    public class Attendee
+    {
+        public string FullName { get; set; } = "";
+        public string Phone { get; set; } = "";
+        public string Location { get; set; } = "";
+        public DateTime Timestamp { get; set; }
+        public bool IsFirstTimer { get; set; }
+    }
+}
--- DestinyLineWebApp/DestinyLineWebApp.Client/Program.cs
+++ DestinyLineWebApp/DestinyLineWebApp.Client/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Components.Web;
 using DestinyLineWebApp.Client;
 using DestinyLineWebApp.Client.Services;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -5,4 +6,5 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.Services.AddSingleton<IMemberService, MemberService>();
 builder.RootComponents.Add<Routes>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 await builder.Build().RunAsync();
